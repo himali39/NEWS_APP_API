02c5dc5ecc9c32c76bd48ed12b826906db27c15d
himali39 <hg.idea2code@gmail.com>
2024-01-03 12:10:32 +0530

    apis done

--- News_app/src/Api/routes/app/appCategoryRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getCategory } = require("../../controllers/app/appCategoryController");
-
-const router = express.Router();
-
-
-router.get("/get-category", getCategory);
-
-module.exports = router;
---
+++ main/Api/src/routes/app/appLanguageRouter.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {
+  getLanguage,
+ 
+} = require("../../controllers/app/appLanguageController");
+const verifyAppToken = require("../../helper/verifyAppToken");
+
+const router = express.Router();
+
+/* -------------------------- list of Language data ------------------------- */
+router.get("/get-Language", verifyAppToken, getLanguage);
+
+
+module.exports = router;
